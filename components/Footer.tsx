b2fa903205e6de684ecfd6bd2cbf4504566f7ccb
Mohit Srivastava <mohit@Onebyzero.ai>
2024-03-11 12:37:32 +0530

    Addec hanges to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,8 +36,8 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright {currentYear} {config.author}</div>
-
+      <div className={styles.copyright}>© {currentYear} {config.author}</div>
+{/* 
       <div className={styles.settings}>
         {hasMounted && (
           <a
@@ -50,7 +50,7 @@ export const FooterImpl: React.FC = () => {
             {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
           </a>
         )}
-      </div>
+      </div> */}
 
       <div className={styles.social}>
         {config.twitter && (
