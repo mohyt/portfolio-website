8912a3f4a160af4911aa1a46da5e0c6460a48395
Mohit Srivastava <mohit@Onebyzero.ai>
2024-06-04 07:26:48 +0530

    CHanged notion

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '66d67116128a46a69a37352fd12be08a',
+  rootNotionPageId: 'e4293a2a7fed44da92e8b690ae9addf3',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
